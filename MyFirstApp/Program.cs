5671dcca6f359c6811d7a7836cdf899893f180cb
Anvor <anvor.arak@karlstorz.com>
2023-05-02 22:00:16 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Rakendus palub kasutajal sisestada enda nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //Output
+//string - sõna
+string userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //Output
+
+
+
+
